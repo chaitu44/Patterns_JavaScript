f6fe1a6b86eae313fd1517fff1dd51736fbdb226
chaitu44 <chaitu44sap@gmail.com>
2013-09-28 13:52:53 +0530

    zzxczx

--- Factory/add_all_using ducktyping.js
+++ Factory/add_all_using ducktyping.js
@@ -18,7 +18,12 @@ function add(){
 		if(n instanceof Array) return add.apply(this,n);
 		return n;
 	}
-
+function getRemaining(args){
+	var remaining = [];
+	for (var i=1; i<args.length;i++)
+		remaining.push(args[i]);
+	return remaining;
+}
 	var result = 0;
 	for(var i=0; i<arguments.length;i++)
 		result += parseArg(arguments[i]);
@@ -27,3 +32,14 @@ function add(){
 }
 
 
+function add(){
+	function parseArg(n){
+		if(typeof n === "string") return parseInt(n,10);
+		if(typeof n === "function") return parseArg(n());
+		
+		if(n instanceof Array) return add.apply(this,n);
+		return n;
+	}
+
+return arguments.length === 1 ? parseArg(arguments][0]) : parseArg(arguments[0]) + add.apply(this, Array.prototype.call(arguments,1));
+}
